07b70358fb7e1468eae5d2b5def5c926ce0177ab
WBertan <william.bertan@gmail.com>
2020-01-19 17:16:02 +0000

    Finish project.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,36 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBAction func askButtonPressed(_ askButton: UIButton) {
+        askButton.isEnabled = false
+        askButton.fadeOut()
+        askButton.setBackgroundImage(ballArray.randomElement(), for: .normal)
+        askButton.fadeIn(2.0)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            askButton.fadeOut()
+            askButton.setBackgroundImage(#imageLiteral(resourceName: "ball0"), for: .normal)
+            askButton.fadeIn(1.0)
+            askButton.isEnabled = true
+        }
+    }
+    
 }
 
+extension UIView {
+    func fadeTo(_ alpha: CGFloat, duration: TimeInterval = 0.3) {
+        DispatchQueue.main.async {
+            UIView.animate(withDuration: duration) {
+                self.alpha = alpha
+            }
+        }
+    }
+    
+    func fadeIn(_ duration: TimeInterval = 0.3) {
+        fadeTo(1.0, duration: duration)
+    }
+    
+    func fadeOut(_ duration: TimeInterval = 0.3) {
+        fadeTo(0.0, duration: duration)
+    }
+}
